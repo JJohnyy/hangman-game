30633af4e5372e2ce490792b798d37de89fce4fa
JJohnyy <jan.jasa@email.cz>
2022-01-14 15:11:24 +0000

    added function for guessing word from letters and updated readme

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Hangman
 
-Hangman gamme is a Python terminal game
+* Hangman gamme is a Python terminal game
 
-Users can try to find a correct word in 6 steps to win a game.
-Otherwise they will get hanged and lose.
+* Users can try to find a correct word in 6 steps to win a game.
+
+* Otherwise they will get hanged and lose.
+
+
 
 ## Here is a version of the game
 
@@ -11,7 +14,9 @@ Otherwise they will get hanged and lose.
 
 # How to play the game
 
-hangman is a classic game played from the youngest age especiall at elementary school, game improves vocabulary mastery
-In this version, playes enters their name
-The playes can see the game(displayed hangman and a line where he can guess the letter in a word which is uknown and randomly generated from the list of words)
+* hangman is a classic game played from the youngest age especiall at elementary school, game improves vocabulary mastery
+
+* In this version, playes enters their name
+
+* The playes can see the game(displayed hangman and a line where he can guess the letter in a word which is uknown and randomly generated from the list of words)
 
--- run.py
+++ run.py
@@ -4,30 +4,47 @@
 
 import random
 from words import word_list
+from game import hangman_stages
 
-player_name = input("Hello player, enter your name: ").upper()
-print(player_name)
 
+
+
+player_name = input("Hello players, please enter your name: ")
+ 
 
 def get_word():
-    """
-    get word from from a list and convert it to uppercase
-    """
     word = random.choice(word_list)
     return word.upper()
 
-
-def play(word):
-    """
-    setup game
-    """
+def game(word):
     word_completion = "_" * len(word)
-    guessed = False
+    guessed = false
     guessed_letters = []
     guessed_words = []
     tries = 6
-    print(f"hello {player_name}, lets play Hangman")
-    print(display_hangman(tries))
+    print(f"Hello {player_name}, lets play hangman")
+    print(hangman_stages(tries))
     print(word_completion)
     print("\n")
+    while not guessed and tries > 0:
+        guess = input("please guess a letter or word: ").upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print(f"Hey, {player_name} you already guessed the letter", guess)
+            elif guess not in word:
+                print(guess, "is not in the word")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print(f"good job {player_name}", guess, "is in the word")
+                guessed_letters.append(guess)
+                word_list = list(word_completion)
+                symbols = [i for i, letter in enumerate(word) if letter == guess]
+                for index in symbols:
+                    word_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if"_" not in word_completion:
+                    guessed = True
+
+
 
